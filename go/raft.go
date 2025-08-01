64423916494810802eec1247a3a1ede8949a24d2
FrancescoLuzzi <francescoluzzi99@libero.it>
2025-08-01 21:46:27 +0200

    wip(go): added comments and impl

--- go/raft.go
+++ go/raft.go
@@ -6,24 +6,33 @@ import (
 	raft "github.com/FrancescoLuzzi/raft/raft"
 )
 
-type RaftModuleService interface {
-	AppendEntries(context.Context, *raft.AppendEntriesRequest) (*raft.AppendEntriesResponse, error)
-	RequestVote(context.Context, *raft.RequestVoteRequest) (*raft.RequestVoteResponse, error)
-	InstallSnapshot(context.Context, *raft.InstallSnapshotRequest) (*raft.InstallSnapshotResponse, error)
+type RaftModuleClient interface {
+	AppendEntries(ctx context.Context, nodeId uint32, request *raft.AppendEntriesRequest) (*raft.AppendEntriesResponse, error)
+	RequestVote(ctx context.Context, nodeId uint32, request *raft.RequestVoteRequest) (*raft.RequestVoteResponse, error)
+	InstallSnapshot(ctx context.Context, nodeId uint32, request *raft.InstallSnapshotRequest) (*raft.InstallSnapshotResponse, error)
 }
 
+type RaftModuleService interface {
+	AppendEntries(ctx context.Context, request *raft.AppendEntriesRequest) (*raft.AppendEntriesResponse, error)
+	RequestVote(ctx context.Context, request *raft.RequestVoteRequest) (*raft.RequestVoteResponse, error)
+	InstallSnapshot(ctx context.Context, request *raft.InstallSnapshotRequest) (*raft.InstallSnapshotResponse, error)
+}
+
+// TODO: add randomization by looking up an environment variable
+// or by creating a second implmentation for testing
 type server struct {
 	raft.UnimplementedRaftModuleServiceServer
+	rms RaftModuleService
 }
 
-var _ RaftModuleService = &server{}
+var _ raft.RaftModuleServiceServer = &server{}
 
-func (s *server) AppendEntries(context.Context, *raft.AppendEntriesRequest) (*raft.AppendEntriesResponse, error) {
-	return nil, nil
+func (s *server) AppendEntries(ctx context.Context, request *raft.AppendEntriesRequest) (*raft.AppendEntriesResponse, error) {
+	return s.rms.AppendEntries(ctx, request)
 }
-func (s *server) RequestVote(context.Context, *raft.RequestVoteRequest) (*raft.RequestVoteResponse, error) {
-	return nil, nil
+func (s *server) RequestVote(ctx context.Context, request *raft.RequestVoteRequest) (*raft.RequestVoteResponse, error) {
+	return s.rms.RequestVote(ctx, request)
 }
-func (s *server) InstallSnapshot(context.Context, *raft.InstallSnapshotRequest) (*raft.InstallSnapshotResponse, error) {
-	return nil, nil
+func (s *server) InstallSnapshot(ctx context.Context, request *raft.InstallSnapshotRequest) (*raft.InstallSnapshotResponse, error) {
+	return s.rms.InstallSnapshot(ctx, request)
 }
